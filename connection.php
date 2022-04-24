25e4778253ef679bf1bc249301285420dd0f6d91
Miftahur23 <miftahur109970@example.com>
2022-04-24 03:24:38 +0000

    inserted data to db

---
+++ connection.php
@@ -0,0 +1,27 @@
+<?php
+        //initial variables
+        $id = 0;
+        $name = "";
+        $email = "";
+        $country = "";
+
+        //connect to database
+        $db = mysqli_connect('localhost' , 'root' , 'Siam@2022' , 'crudphp');
+
+        //when submit button is clicked
+        if (isset($_POST['submit'])) {
+            
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $country = $_POST['country'];
+
+        $query = "INSERT INTO users (name, email, country) VALUES ('$name', '$email', '$country')";
+        mysqli_query($db, $query);
+
+        //redirecting to previous page
+        // header('location : index.php');
+        header('Location: ' . $_SERVER['HTTP_REFERER']); 
+
+        }
+
+?>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php include('connection.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP CRUD</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Country</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+                <td>1</td>
+                <td>Siam</td>
+                <td>siam@gmail.com</td>
+                <td>Bangladesh</td>
+                <td>
+                    <a href="#" class="edit_btn" >Edit</a>
+                    <a href="#" class="del_btn" >Delete </a>
+                </td>
+        </tbody>
+    </table>
+
+    
+
+    <form method="post" action="connection.php" >
+            <div class="input-group">
+                <label>Name</label>
+                <input type="name" name="name" value="">
+            </div>
+            <div class="input-group">
+                <label>Email</label>
+                <input type="email" name="email" value="">
+            </div>      
+            <div class="input-group">
+                <label>Country</label>
+                <input type="text" name="country" value="">
+            </div>
+            <div class="input-group">
+                <button class="btn" type="submit" name="submit" >Submit</button>
+            </div>
+	</form>
+    
+</body>
+</html>
